ad07d8ea4909208697085291e855fe2c0a9e90ff
_chandrashekher_ <cpsaw999041@gmail.com>
2023-11-09 22:54:10 +0530

    Input code

---
+++ FirstJava Program1/src/input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; // For taking input
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = sc.nextInt();  // nextInt is used for taking Integer value
+        System.out.println("Number :" +num);
+
+//        String name = sc.next();  // next() is used for taking String value
+//        System.out.println(name);
+//
+//        float fl = sc.nextFloat();  // nextFloat() is used for taking float value
+//        System.out.println(fl);
+
+
+    }
+}
